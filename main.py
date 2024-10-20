00958a6e8ca085e4fc128379a3c978353a41bc27
VladimirPopov23 <volodyaklever@gmail.com>
2024-10-20 19:48:20 +0300

    module_5_2.py
    20.10.2024 Задача "Магические здания"

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-# module_5_1.py
-# 20.10.2024 Испр. Задача "Developer - не только разработчик"
+# module_5_2.py
+# 20.10.2024 Задача "Магические здания"
 
 
 class House:
-    def __init__(self, name, number):
+    def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = number
+        self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor):
-        if new_floor > self.number_of_floors or new_floor < 1:
-            print('Такого этажа не существует')
-        else:
-            for i in range(1, new_floor + 1):
-                print(i)
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return title
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
